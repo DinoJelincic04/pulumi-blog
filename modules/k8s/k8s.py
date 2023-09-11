12f9ea10a65583f799aee434de85fadb00c1315e
DinoJelincic04 <dino.jelincic@wearenotch.com>
2023-09-11 12:27:58 +0200

    add github action

--- modules/k8s/k8s.py
+++ modules/k8s/k8s.py
@@ -4,4 +4,4 @@ import modules.eks.eks as eks
 
 
 
-eks_provider = k8s.Provider("eks-provider", kubeconfig=eks.eks_cluster.kubeconfig_json)
+#eks_provider = k8s.Provider("eks-provider", kubeconfig=eks.eks_cluster.kubeconfig_json)
