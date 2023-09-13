6fe4a16328f1ebdbc63645a63c7c16292ce7821a
DinoJelincic04 <dino.jelincic@wearenotch.com>
2023-09-13 07:55:33 +0200

    change dockerfile for test

--- modules/ecr/ecr.py
+++ modules/ecr/ecr.py
@@ -10,7 +10,34 @@ ecr = aws.ecr.Repository("blog-repo",
         image_tag_mutability="IMMUTABLE")
 
 
-#repository = ecr.Repository("myrepository")
+# repository_policy = aws.ecr.RepositoryPolicy(
+#     "myrepositorypolicy",
+#     repository=ecr.id,
+#     policy=json.dumps({
+#         "Version": "2012-10-17",
+#         "Statement": [{
+#             "Sid": "new policy",
+#             "Effect": "Allow",
+#             "Principal": "*",
+#             "Action": [
+#                 "ecr:GetDownloadUrlForLayer",
+#                 "ecr:BatchGetImage",
+#                 "ecr:BatchCheckLayerAvailability",
+#                 "ecr:PutImage",
+#                 "ecr:InitiateLayerUpload",
+#                 "ecr:UploadLayerPart",
+#                 "ecr:CompleteLayerUpload",
+#                 "ecr:DescribeRepositories",
+#                 "ecr:GetRepositoryPolicy",
+#                 "ecr:ListImages",
+#                 "ecr:DeleteRepository",
+#                 "ecr:BatchDeleteImage",
+#                 "ecr:SetRepositoryPolicy",
+#                 "ecr:DeleteRepositoryPolicy",
+#             ]
+#         }]
+#     })
+# )
 
 repository_policy = aws.ecr.RepositoryPolicy(
     "myrepositorypolicy",
@@ -22,20 +49,7 @@ repository_policy = aws.ecr.RepositoryPolicy(
             "Effect": "Allow",
             "Principal": "*",
             "Action": [
-                "ecr:GetDownloadUrlForLayer",
-                "ecr:BatchGetImage",
-                "ecr:BatchCheckLayerAvailability",
-                "ecr:PutImage",
-                "ecr:InitiateLayerUpload",
-                "ecr:UploadLayerPart",
-                "ecr:CompleteLayerUpload",
-                "ecr:DescribeRepositories",
-                "ecr:GetRepositoryPolicy",
-                "ecr:ListImages",
-                "ecr:DeleteRepository",
-                "ecr:BatchDeleteImage",
-                "ecr:SetRepositoryPolicy",
-                "ecr:DeleteRepositoryPolicy",
+                "ecr:*",
             ]
         }]
     })
